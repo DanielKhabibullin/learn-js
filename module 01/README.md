b3c9d710caea745ea69ba5548b0b0a9510a51d10
Daniel Khabibullin <i6unny@gmail.com>
2023-02-07 14:13:41 +0500

    first commit

---
+++ module 01/README.md
@@ -0,0 +1,7 @@
+### Description
+# Just homework for JS course
+- uploaded <a href="" target="_blank">here<a>
+
+//- webpack
+//- scss
+//- responsive
---
+++ module 01/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const productName = 'pencil';
+const productCount = 10;
+const productCategory = 'office';
+const productPrice = 20;
+
+console.log('productName: ', productName);
+
+
+const sumTotal = productCount * productPrice;
+console.log('sumTotal: ', sumTotal);
