3a6b19f996440450b229a6e23a297a2c68ba9d98
Daniel Khabibullin <i6unny@gmail.com>
2023-02-08 16:36:27 +0500

    taskHard_1 & _2 modified

--- einstein.js
+++
@@ -1,5 +0,0 @@
-const myWeight = 83;
-const speedLight = 3e8;
-
-const energy = BigInt(myWeight * (speedLight ** 2));
-console.log('energy: ', energy);
--- taskHard_1.js
+++ taskHard_1.js
@@ -1,16 +1,19 @@
 'use strict';
 
 const income = +prompt('Enter your income in rubles')?.trim();
-
-if (income >= 50000) {
-	let taxes = income * .3;
-	console.log(`Your taxes are ${taxes} rub`);
+if (isNaN(income)) {
+	alert('Please, enter correct data');
 }
-if (income >= 15000 && income < 50000) {
-	let taxes = income * .2;
-	console.log(`Your taxes are ${taxes} rub`);
+const calculateTax = (income) => {
+	if (income < 15000) {
+		let taxes = income * .13;
+		console.log(`Your taxes are ${taxes} rub`);
+	} else if (income >= 50000) {
+		let taxes = income * .3;
+		console.log(`Your taxes are ${taxes} rub`);
+	} else if (income >= 15000) {
+		let taxes = income * .2;
+		console.log(`Your taxes are ${taxes} rub`);
+	}
 }
-if (income < 15000) {
-	let taxes = income * .13;
-	console.log(`Your taxes are ${taxes} rub`);
-}
+calculateTax(income);
