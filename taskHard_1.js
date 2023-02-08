7ddcc02d04761af9ee4e2f50fe7e688f921c7b13
Daniel Khabibullin <i6unny@gmail.com>
2023-02-08 18:58:20 +0500

    some changes

--- taskHard_1.js
+++ taskHard_1.js
@@ -2,15 +2,18 @@
 
 const income = +prompt('Enter your income in rubles')?.trim();
 
-if (income >= 50000) {
+if (isNaN(income)) {
+	alert('Please, enter correct data');
+}
+if (income > 50000) {
 	let taxes = income * .3;
 	console.log(`Your taxes are ${taxes} rub`);
 }
-if (income >= 15000 && income < 50000) {
+if (income > 15000 && income <= 50000) {
 	let taxes = income * .2;
 	console.log(`Your taxes are ${taxes} rub`);
 }
-if (income < 15000) {
+if (income <= 15000) {
 	let taxes = income * .13;
 	console.log(`Your taxes are ${taxes} rub`);
 }
--- taskHard_2.js
+++ taskHard_2.js
@@ -1,15 +1,19 @@
 'use strict';
+const income2 = +prompt('Enter your income in rubles')?.trim();
 
-const income = +prompt('Enter your income in rubles')?.trim();
-if (income >= 50000) {
-	let taxes = 15000 * .13 + 35000 * .2 + (income - 50000) * .3;
+if (isNaN(income2)) {
+	alert('Please, enter correct data');
+}
+
+if (income2 > 50000) {
+	let taxes = 15000 * .13 + 35000 * .2 + (income2 - 50000) * .3;
 	console.log(`Your taxes are ${taxes} rub`);
 }
-if (income >= 15000 && income < 50000) {
-	let taxes = 15000 * .13 + (income - 15000) * .2;
+if (income2 > 15000 && income2 <= 50000) {
+	let taxes = 15000 * .13 + (income2 - 15000) * .2;
 	console.log(`Your taxes are ${taxes} rub`);
 }
-if (income < 15000) {
-	let taxes = income * .13;
+if (income2 <= 15000) {
+	let taxes = income2 * .13;
 	console.log(`Your taxes are ${taxes} rub`);
 }
