7db82cb8a9bd33368ef3af90901fbb2ac44bb119
Daniel Khabibullin <i6unny@gmail.com>
2023-02-08 15:00:49 +0500

    script.js modified

--- script.js
+++ script.js
@@ -27,19 +27,26 @@
 {
 	const productName = prompt('Enter product name')?.trim();
 	const productCount = +prompt('Enter product qty')?.trim();
+	if (isNaN(productCount)) {
+		alert('Please, enter correct data');
+	}
 	const productCategory = prompt('Enter product category')?.trim();
 	const productPrice = +prompt('Enter product price, rub')?.trim();
+	if (isNaN(productPrice)) {
+		alert('Please, enter correct data');
+	}
 
 	const sumTotal = productCount * productPrice;
 
-	if (isNaN(productCount) || isNaN(productPrice)) {
-		alert('Please, enter correct data');
-		} else if (productCount > 1) {
-			console.log(`"There are ${productCount} units of "${productName}" worth ${sumTotal} rubles in stock"`);
-		} 
-		if (productCount == 1) {
-			console.log(`"There is 1 unit of "${productName}" worth ${productCount} rubles in stock"`);
-		};
+	if (productCount > 1) {
+		console.log(`"There are ${productCount} units of "${productName}" worth ${sumTotal} rubles in stock"`);
+	} 
+	if (productCount == 1) {
+		console.log(`"There is 1 unit of "${productName}" worth ${productCount} rubles in stock"`);
+	};
+	if (productCount < 1) {
+		console.log('Something gone wrong');
+	};
 };
 
 // ?? Анна, посмотри, пожалуйста, почему если заменить на условие ниже, то не работает код
