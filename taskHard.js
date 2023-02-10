908ecbb37d436ec226f54bf20de480c294d713ae
Daniel Khabibullin <i6unny@gmail.com>
2023-02-10 23:28:14 +0500

    Module01_Theme07 hard hw added

--- taskHard.js
+++ taskHard.js
@@ -10,8 +10,34 @@ const allСashbox = [
   [6, 13900], 
   [1, 370]
 ];
+// for (let i of allСashbox) {
+//   for (let j of i) {
+//     console.log(j);
+//   }
+// }
+const getAveragePriceGoods = (arr) => {
 
-const  getAveragePriceGoods = () => {
-	// newArr = 2 элемент / 1 элемент
-	// проссуммировать и разделить на длину
-}
+	let result = 0;
+	let sum = 0;
+	for(let i = 0; i < arr.length; i++){
+		sum += ( arr[i][1] / arr[i][0]);
+		console.log('sum: ', sum);
+	}
+	return result = sum / arr.length;
+};
+
+console.log(getAveragePriceGoods(allСashbox));
+
+// const getAveragePriceGoods1 = (arr) => {
+
+// 	let result = 0;
+// 	let sum = 0;
+// 	arr.forEach((count) => {
+// 		count.forEach((price) => {
+		
+// 			});
+// 	});
+// 	return result = sum / arr.length;
+// };
+
+// console.log(getAveragePriceGoods1(allСashbox));
