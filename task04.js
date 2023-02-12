a4d404eb722a6d0474dba7d9a1445d05b65c9f9e
Daniel Khabibullin <i6unny@gmail.com>
2023-02-12 21:33:56 +0500

    task04 fixed

--- task04.js
+++ task04.js
@@ -7,13 +7,13 @@ function chooseYears(m, n) {
 		return chooseYears(n, m);
 	}
 	for (let i = m; i <= n; i++) {
-		if (i % 4 === 0) {
+		if ((i % 4 === 0 && i % 100 !== 0) || (i % 100 === 0 && i % 400 === 0)) {
 		arr.push(i);
 		}
 	}
 	return arr;
 };
 
-console.log('chooseYears: ', chooseYears(-1500, -1400));
+console.log('chooseYears: ', chooseYears(2000, 2150));
 
 
