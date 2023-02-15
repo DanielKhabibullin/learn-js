aeb0e0fc97681bad36ed3fcb32222ed755da5b23
Daniel Khabibullin <i6unny@gmail.com>
2023-02-15 18:17:11 +0500

    task01/02_added

--- task01.js
+++ task01.js
@@ -1,10 +1,39 @@
 'use strict';
 
-const generateArr1 = (a) => {
-	let result1 = [];
-	for (let i = 0; i < a; i++ ) {
-		result1[i] = Math.round(Math.random() * 100);
-	};
-	return result1;
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	let number = 0;
+	number = Math.floor(Math.random() * (max - min + 1)) + min;
+	// Максимум и минимум включаются
+	return number;
 }
-console.log(generateArr1(100));
+
+const number = getRandomIntInclusive(0, 100);
+console.log('number: ', number);
+
+function isNum(num) {
+	return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+const guessNumber = () => {
+	let userNumber = prompt('Enter number from 0 to 100');
+	if (userNumber === null) {
+		alert('Come back again');
+	} else if (isNum(userNumber)) {
+		userNumber = +userNumber;
+		if (parseInt(userNumber) > number) {
+			alert('Less!');
+		} else if (parseInt(userNumber) < number) {
+			alert('More!');
+		} else if (parseInt(userNumber) === number) {
+			alert('Good job');
+			return;
+		} else {
+			alert('Please, enter correct number');
+		}
+		return guessNumber();
+	}
+};
+
+guessNumber();
--- task02.js
+++ task02.js
@@ -1,19 +1,26 @@
+'use strict';
+
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-	let number2 = 0;
-  number2 = Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-	return number2;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	let number = 0;
+	number = Math.floor(Math.random() * (max - min + 1)) + min;
+	// Максимум и минимум включаются
+	return number;
+}
+
+const addRandomNumber = (arr) => {
+	const number = getRandomIntInclusive(0, 10);
+	const newArr = arr.slice(0);
+	newArr.push(number);
+	const result = newArr.reduce((sum, elem) => sum + elem, 0);
+	if (result <= 50) {
+		return addRandomNumber(newArr);
+	} else {
+		return newArr;
+	}
 };
 
-const generateArr2 = (a, m, n) => {
-	let result2 = [];
-	if (n < m) {
-		return generateArr2(a, n, m);
-	} 
-	for (let i = 0; i < a; i++ ) {
-			result2[i] = getRandomIntInclusive(m, n);
-	}
-	return result2;
-}
-console.log(generateArr2(20, 100, -10));
+const array = [1, 2, 3];
+addRandomNumber(array);
+console.log(addRandomNumber(array));
