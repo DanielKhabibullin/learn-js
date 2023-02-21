5d635163c4da34e41d35e9325c0f13af7e7ee955
Daniel Khabibullin <i6unny@gmail.com>
2023-02-21 22:00:06 +0500

    fix added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ const propslists = document.querySelectorAll('.props__list');
 const content = document.querySelectorAll('.content');
 const propsItemsTwo = document.querySelectorAll('.props__item_two');
 const propsItemsFour = document.querySelectorAll('.props__item_four');
+const itemTitle = document.querySelectorAll('.item__title');
+console.log('itemTitle: ', itemTitle);
 
 ads.remove();
 itemFive.before(itemFour);
@@ -15,7 +17,12 @@ propsItemsTwo[7].after(propsItemsTwo[8]);
 propsItemsTwo[8].after(propsItemsTwo[9]);
 
 propslists[4].remove();
+console.log('propslists: ', propslists);
 propslists[3].remove();
 content[3].append(propslists[4]);
 content[4].append(propslists[3]);
 
+propslists[5].before(itemTitle[1]); // es6
+propslists[2].before(itemTitle[3]);
+propslists[3].before(itemTitle[4]);
+itemTitle[2].textContent = 'This и прототипы объектов';
