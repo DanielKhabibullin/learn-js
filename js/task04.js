bcf6142a1640238d6a9bdc1930d15f2e4e75a72b
Daniel Khabibullin <i6unny@gmail.com>
2023-03-18 20:46:03 +0500

    Add fix

--- js/task04.js
+++ js/task04.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const replaceUrls = url => {
-	const regexp = /(https?:\/\/\S+)/gi;
-	return url.replace(regexp, '<a href="$1">$1</a>');
+	const regexp = /(https?:\/\/)(www\.)?(\S+)/gi;
+	return url.replace(regexp, (match, p1, p2, p3) => {
+		const domain = p2 ? p3 : p2;
+		return `<a href="${p1}${domain}">${p1}${domain}</a>`;
+	});
 };
 
-const text = 'Посетите мой сайт http://site.ru или мой блог https://site.com/blog';
+const text = 'Посетите мой сайт http://www.site.ru или мой блог https://www.site.com/blog';
 const replacedText = replaceUrls(text);
 console.log(replacedText);
