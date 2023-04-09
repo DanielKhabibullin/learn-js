f5d1fa8fd6df3cbf151887a6a6660732f0c40b65
Daniel Khabibullin <i6unny@gmail.com>
2023-04-09 18:31:01 +0500

    add some fix

--- constructors.js
+++ constructors.js
@@ -1,83 +1,89 @@
-function Cart(items = []) {
+const Cart = function(items = []) {
 	this.goods = items;
 	this.totalPrice = 0;
 	this.count = 0;
 	this.discount = 0;
-
-	this.calculateGoodsPrice = function() {
-		this.totalPrice = this.goods.reduce(function(total, item) {
-			return total + item.price - (item.price * item.discount);}, 0);
-		this.increaseCount();
-		
-	};
-	
-	this.addGoods = function(item) {
-		this.goods.push(item);
-		this.increaseCount();
-		this.calculateGoodsPrice();
-	};
-
-	this.getTotalPrice = function() {
-		return this.totalPrice;
-	};
-
-	this.increaseCount = function() {
-		this.count++;
-	};
-
-	this.clear = function() {
-		this.goods = [];
-		this.totalPrice = 0;
-		this.count = 0;
-	};
-
-	this.print = function() {
-		console.log(JSON.stringify(this.goods));
-		console.log(`Total price: ${this.getTotalPrice()}`);
-	};
+	// this.items = {
+	// 	goods: [],
+	// 	totalPrice: 0,
+	// 	count: 0,
+	// }
 };
 
-function Goods(price, name, discount) {
+Cart.prototype.calculateGoodsPrice = function() {
+	this.totalPrice = this.goods.reduce(function(total, item) {
+		return total + item.price - (item.price * item.discount / 100);}, 0);
+};
+
+Cart.prototype.addGoods = function(item) {
+	this.goods.push(item);
+	this.increaseCount();
+	this.calculateGoodsPrice();
+};
+
+Cart.prototype.getTotalPrice = function() {
+	return this.totalPrice;
+};
+
+Cart.prototype.increaseCount = function() {
+	this.count++;
+};
+
+Cart.prototype.clear = function() {
+	this.goods = [];
+	this.totalPrice = 0;
+	this.count = 0;
+};
+
+Cart.prototype.print = function() {
+	console.log(JSON.stringify(this.goods));
+	console.log(`Total price: ${this.getTotalPrice()}`);
+};
+
+const Goods = function(price, name, discount) {
 	this.price = price;
 	this.name = name;
 	this.discount = discount;
-}
+};
 
-function FoodGoods(price, name, discount, calories) {
+Object.setPrototypeOf(Goods.prototype, Cart.prototype);
+
+const FoodGoods = function(price, name, discount = 0, calories = '') {
 	Goods.call(this, price, name, discount);
 	this.calories = calories;
-}
+};
 
-Object.setPrototypeOf(FoodGoods.prototype, Goods.prototype);
-FoodGoods.prototype.constructor = FoodGoods;
+Object.setPrototypeOf(FoodGoods.prototype, Goods.prototype); // чтобы milk являлось экземпляром класса goods
 
-function ClothingGoods(price, name, discount, material) {
+const ClothingGoods = function(price, name, discount = 0, material = '') {
 	Goods.call(this, price, name, discount);
 	this.material = material;
-}
+};
 
 Object.setPrototypeOf(ClothingGoods.prototype, Goods.prototype);
-ClothingGoods.prototype.constructor = ClothingGoods;
 
-function TechnicsGoods(price, name, discount, techType) {
+const TechnicsGoods = function(price, name, discount = 0, techType = '') {
 	Goods.call(this, price, name, discount);
 	this.techType = techType;
-}
+};
 
 Object.setPrototypeOf(TechnicsGoods.prototype, Goods.prototype);
-TechnicsGoods.prototype.constructor = TechnicsGoods;
 
+const iphone = new Goods(1000, 'iphone 14', 10);
+console.log(iphone);
 const milk = new FoodGoods(2.5, 'Milk', 0, 120);
-console.log('milk: ', milk);
-const tShirt = new ClothingGoods(15, 'T-shirt', 0.1, 'cotton');
-const smartphone = new TechnicsGoods(600, 'Smartphone', 0.2, 'smartphone');
+console.log(milk);
+const tShirt = new ClothingGoods(15, 'T-shirt', 10, 'cotton');
+const smartphone = new TechnicsGoods(600, 'Smartphone', 10);
 
 const cart = new Cart();
 
 cart.addGoods(milk);
 cart.addGoods(tShirt);
 cart.addGoods(smartphone);
+cart.addGoods(iphone);
 
 cart.print();
 
+console.log(milk instanceof Cart);
 console.dir(cart);
